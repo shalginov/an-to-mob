8d9cfd1ea4af69a52edbfbce1feeee10b527b842
VS <slava8427@mail.ru>
2020-04-14 10:02:17 +0700

    add inintial app

--- todoist/src/app/app.module.ts
+++ todoist/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { CabComponent } from './cab/cab.component';
+import { TicComponent } from './tic/tic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    CabComponent,
+    TicComponent
   ],
   imports: [
     BrowserModule,
